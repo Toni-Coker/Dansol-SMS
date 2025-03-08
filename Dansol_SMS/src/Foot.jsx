2881185a1388c2d45c4d17dce204f35fb086ce7c
TboyxfireXD <oluwatonilobacoker@gmail.com>
2025-03-08 09:12:15 +0100

    added react icons, udated footer, udated app.css, updated foot.css, updated footer

--- Dansol_SMS/src/Foot.jsx
+++ Dansol_SMS/src/Foot.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
 import "./Foot.css";
+
 const Foot = () => {
   const year = new Date().getFullYear();
   return (
@@ -12,7 +16,17 @@ const Foot = () => {
           </p>
         </div>
         <div className="social">
-            
+          <ul>
+            <li>
+              <FaInstagram size={50} color="#f0f0f0" />
+            </li>
+            <li>
+            <FaFacebookSquare size={50} color="#f0f0f0" />
+            </li>
+            <li>
+              <TbWorldWww size={50} color="#f0f0f0" />
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
