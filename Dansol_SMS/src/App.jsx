bc8c0fc9f3f1701baab32b6f9fe9b057711cee2f
TboyxfireXD <oluwatonilobacoker@gmail.com>
2025-03-08 08:28:29 +0100

    added routing, created home and error page

--- Dansol_SMS/src/App.jsx
+++ Dansol_SMS/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import ErrorPage from './ErrorPage'
 
 const App = () => {
   return (
     <div>
-      
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<ErrorPage />}></Route>
+      </Routes>
+    </Router>
     </div>
   )
 }
